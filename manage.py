f0d7ba75bb70d43346bf17dc80e509319ac80be5
hameddehghani <hamed_de@yahoo.com>
2020-05-26 12:13:42 +0430

    api test

---
+++ manage.py
@@ -0,0 +1,16 @@
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+from app import app, db
+from entities import features, variant_price, users, favorites
+
+# python manage.py db init
+# python manage.py db migrate
+# python manage.py db upgrade
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
+    db.create_all()
